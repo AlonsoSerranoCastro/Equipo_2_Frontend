35269560465261af3ea6f91a8f0fb03004f97644
Sorck <juanluis021001@gmail.com>
2023-04-13 01:28:24 -0600

    Se realizaron cambios en los nombres de los archivos, se comentó por completo la página para mejorar la lectura del código, y se realizó la importación de las fuentes en google.

--- js/contacto.js
+++ js/contacto.js
@@ -1,3 +1,4 @@
+//Se definen las variables totales a utilizar en este JavaScript
 let txtNombre = document.getElementById("nombre");
 let txtEmail = document.getElementById("email");
 let txtTelefono = document.getElementById("telefono");
@@ -9,95 +10,104 @@ let alerta = document.getElementById("alertValidaciones");
 let txtAlerta = document.getElementById("alertValidacionesTexto");
 let msgError = "";
 
+//Listener acerca de los diferentes campos a rellenar en el formulario de contacto
 txtNombre.addEventListener("blur", function (event) {
     event.preventDefault();
     txtNombre.value.trim();
 
-    txtEmail.addEventListener("blur", function(event){
+    txtEmail.addEventListener("blur", function (event) {
         event.preventDefault();
-        txtEmail.value = txtEmail.value.trim();});
-    
-    txtTelefono.addEventListener("blur", function(event){
+        txtEmail.value = txtEmail.value.trim();
+    });
+
+    txtTelefono.addEventListener("blur", function (event) {
         event.preventDefault();
-        txtTelefono.value = txtTelefono.value.trim();});
-    
-    txtMensaje.addEventListener("blur", function(event){
+        txtTelefono.value = txtTelefono.value.trim();
+    });
+
+    txtMensaje.addEventListener("blur", function (event) {
         event.preventDefault();
-        txtMensaje.value = txtMensaje.value.trim();});
+        txtMensaje.value = txtMensaje.value.trim();
+    });
 });
 
+//Se definen las constantes con expresiones regulares para poner parámetros de utilización sobre los campos a rellenar del formulario
 const regexName = (/^[A-Za-záéíóúÁÉÍÓÚÑñ\s]{3,}$/);
 const regexEmail = (/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/);
 const regexTelefono = (/^[0-9\s]{10}$/);
 const regexMensaje = (/^[\s\S]{5,150}$/);
 
-// function validarNombre() 
-
+// función para validar los diferentes errores que existen en los campos del formulario y agregarlos a la variable msgError
 btnContacto.addEventListener("click", function (event) {
     event.preventDefault();
-let cont = 0; 
+    let cont = 0;
 
     if (validarNombre(txtNombre.value)) {
 
 
     } else {
         console.log(msgError);
-        cont+= 1;
+        cont += 1;
     }
     if (validarEmail(txtEmail.value)) {
 
     } else {
         console.log(msgError);
-        cont+= 1;
+        cont += 1;
     }
 
     if (validarTelefono(txtTelefono.value)) {
 
     } else {
         console.log(msgError);
-        cont+= 1;
+        cont += 1;
     }
 
     if (validarMensaje(txtMensaje.value)) {
 
     } else {
         console.log(msgError);
-        cont+= 1;
+        cont += 1;
     }
 
     if (validarSucursal(txtSucursales.value)) {
 
     } else {
         console.log(msgError);
-        cont+= 1;
+        cont += 1;
     }
-    if (cont > 0){
+    //Alert insertado mediante la página sweet alert2
+    if (cont > 0) {
         let htmlAlert = `<ul class = listaDeErrores> Tienes errores en los siguientes campos: ${msgError}</ul>`;
-        Swal.fire({ 
-        icon: 'error',
-        title: 'Oraa...',
-        html: htmlAlert,
-      }); 
-      cont=0;
-      htmlAlert = "";
-      msgError="";
+        Swal.fire({
+            icon: 'error',
+            title: 'ORAA...',
+            html: htmlAlert,
+        });
 
-       
-    } else{
-       let sugerencias = "Nombre: " + txtNombre.value  + "\nE-mail: " + txtEmail.value + "\nTeléfono: " + txtTelefono.value + "\nSucursales: " + txtSucursales.value +"\nMensaje: "+ txtMensaje.value; 
-    
-       Email.send({
+        //Funcionalidad para qe se reinicie el contador cada vez que se muestre un error en el envío del formulario de contacto
+        cont = 0;
+        htmlAlert = "";
+        msgError = "";
 
-        SecureToken : "598073ae-0fff-4b3a-bc8d-3de1756015b4",
-        To : 'burgershouse2023@gmail.com',
-        From : "burgershouse2023@gmail.com",
-        Subject : "Buzón de Quejas y Sugerencias BurgersHouse",
-        Body : sugerencias,
-    }).then(
-      message => Swal.fire ("Sugerencia enviada")
-    )}
+        //else que muestra los datos del error mediante las sugerencias
+    } else {
+        let sugerencias = "Nombre: " + txtNombre.value + "\nE-mail: " + txtEmail.value + "\nTeléfono: " + txtTelefono.value + "\nSucursales: " + txtSucursales.value + "\nMensaje: " + txtMensaje.value;
+
+        // Es donde se envía el correo mediante SMTPJS
+        Email.send({
+            SecureToken: "598073ae-0fff-4b3a-bc8d-3de1756015b4",
+            To: 'burgershouse2023@gmail.com',
+            From: "burgershouse2023@gmail.com",
+            Subject: "Buzón de Quejas y Sugerencias BurgersHouse",
+            Body: sugerencias,
+        }).then(
+            message => Swal.fire("Sugerencia enviada")
+        )
+    }
 
 });
+//Función para validar los datos del formulario con los mensajes que muestran los errores en formato de lista desordenada
 function validarNombre(nombre) {
     if (regexName.test(nombre)) {
         return true;
