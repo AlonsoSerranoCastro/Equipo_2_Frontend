d4941a9c640b97034284e18957fb6b83690c0d5f
MTSC <teresa.scontla@gmail.com>
2023-04-13 22:02:10 -0600

    Se agregó archivo html y css del footer, se creó el html de menú.

--- js/navBar.js
+++ js/navBar.js
@@ -1,6 +1,8 @@
 let navToggle = document.querySelector(".navbar-toggler");
 let navMenu = document.querySelector(".navbar-nav");
 
+let btnMenu = document.querySelector(".navMenu");
+
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("navbar-nav_visible");
@@ -11,3 +13,14 @@ navToggle.addEventListener("click", () => {
       navToggle.setAttribute("aria-label", "Abrir menú");
     }
   });
+
+btnMenu.addEventListener("click", function(){
+        window.location.href = "contacto.html";
+    });
+
+// // Cargar Navbar
+// fetch('./contacto.html')
+//     .then(response => response.text())
+//     .then(data => {
+//         document.getElementById('navBarColap').innerHTML = data;
+//     });
