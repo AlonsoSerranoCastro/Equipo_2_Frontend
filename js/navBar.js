4d7fc9244c988caa84a3a7a9ed570098b6d80e89
MTSC <teresa.scontla@gmail.com>
2023-04-13 22:27:18 -0600

    Se agregaron los archivos html de acerca de, carrito de compras,login,promociones y sucursales.También se crearon las variables y botones en el javascript del navbar, para movernos por el sitio web con la barra de navegación.

--- js/navBar.js
+++ js/navBar.js
@@ -2,6 +2,15 @@ let navToggle = document.querySelector(".navbar-toggler");
 let navMenu = document.querySelector(".navbar-nav");
 
 let btnMenu = document.querySelector(".navMenu");
+let btnAcerca = document.querySelector(".navAcerca");
+let btnCarrito = document.querySelector(".bi-cart3");
+let btnContacto = document.querySelector(".navContacto");
+let btnLogin = document.querySelector(".bi-person-circle");
+let btnNosotros = document.querySelector(".navNosotros");
+let btnPromos = document.querySelector(".navPromos");
+let btnSucursales = document.querySelector(".navSucursales");
+let btnLogo = document.querySelector(".logoNav");
+
 
 
 navToggle.addEventListener("click", () => {
@@ -13,9 +22,42 @@ navToggle.addEventListener("click", () => {
       navToggle.setAttribute("aria-label", "Abrir menú");
     }
   });
+  
+
+btnLogo.addEventListener("click", function(){
+    window.location.href = "index.html";
+});
 
 btnMenu.addEventListener("click", function(){
+        window.location.href = "menu.html";
+    });
+
+btnAcerca.addEventListener("click", function(){
+        window.location.href = "acerca.html";
+    });
+
+btnCarrito.addEventListener("click", function(){
+        window.location.href = "carritoCom.html";
+    });
+
+btnContacto.addEventListener("click", function(){
         window.location.href = "contacto.html";
+    });
+
+btnLogin.addEventListener("click", function(){
+        window.location.href = "login.html";
+    });
+
+btnNosotros.addEventListener("click", function(){
+        window.location.href = "nosotros.html";
+    });
+
+btnPromos.addEventListener("click", function(){
+        window.location.href = "promociones.html";
+    });
+
+btnSucursales.addEventListener("click", function(){
+        window.location.href = "sucursales.html";
     });
 
 // // Cargar Navbar
