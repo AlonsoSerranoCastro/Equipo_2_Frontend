15b54d6a457b0de13e7eb723666f4189845cf027
Markinsky <carlosgerardo1957@hotmail.com>
2023-05-13 13:54:30 -0600

    Arreglo de carrito.js y css

--- js/carrito.js
+++ js/carrito.js
@@ -1,73 +1,79 @@
 let divItems=document.getElementById("divItems");
 let hmtlCarrito="";
+var mapCarrito = new Map();
+
 window.addEventListener("load",function(event) {
   divItems.innerHTML = "";
     hmtlCarrito="";
-    let carrito = JSON.parse(localStorage.getItem("jsonCarrito"))
    
-    if (carrito==null) {
-      hmtlCarrito = "Aún no has agregado nada a tu carrito";
-      divItems.insertAdjacentHTML("afterbegin",hmtlCarrito)
-    }
 
-    divItems.innerHTML = "";
-    let syntxCarrito = "";
-    let cont = 1;
-    var mapCarrito = new Map();
-  
-    carrito.forEach(car =>{
-     if(mapCarrito.has(car.nombre)){
-        cont = carrito.filter((element) => element["nombre"] === car.nombre).length;
-     }
-      syntxCarrito = `<div class="card">
-      <img src="${car.imagen}" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title"><strong>${car.nombre}</strong></h5>
-        <p class="card-text"><strong>$ ${car.precio}</strong></p>
-      </div>
-      <i  class="fa-solid fa-plus bg-success"></i>
-      <label>${cont}</label>
-      <i class="fa-solid fa-minus bg-danger"></i> 
-    </div>`;
+    divItems.insertAdjacentHTML("afterbegin",mostrar());
 
-    mapCarrito.set(car.nombre, syntxCarrito);
-    cont = 1;   
-    });
-    mapCarrito.forEach((map,key) =>{
-      hmtlCarrito += mapCarrito.get(key);
-      console.log(hmtlCarrito);
-    });
-
-    divItems.insertAdjacentHTML("afterbegin",hmtlCarrito);
-    /*
-    carrito.forEach(car =>{
-      contadorCarrito.forEach(contCar => {
-        if (car.nombre==contCar) {
-            cont++;
-        }
-        let strId = contCar.replace(/ /g, '_');
-        let inMenu=document.getElementById("cont_" + strId);
-        console.log(inMenu);
-        console.log(cont);
-        console.log("cont_" + strId);
-        console.log(contadorCarrito);
-        //inMenu.innerText = cont;
-      });
-       
-        contadorCarrito.push(car.nombre);
-        stringNombre = car.nombre;
-        var stringId = stringNombre.replace(/ /g, '_');
-        hmtlCarrito += `<div class="card">
-        <img src="${car.imagen}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title"><strong>${car.nombre}</strong></h5>
-          <p class="card-text"><strong>$ ${car.precio}</strong></p>
-        </div>
-        <i  class="fa-solid fa-plus bg-success"></i>
-        <input type="text" id="cont_${stringId}"> 
-        <i class="fa-solid fa-minus bg-danger"></i>
-      </div>`;
-
-    }); */
-   // console.log(hmtlCarrito);
 })
+ function mostrar() {
+  let carrito = JSON.parse(localStorage.getItem("jsonCarrito"))
+   
+  if (carrito==null) {
+    hmtlCarrito = "Aún no has agregado nada a tu carrito";
+    divItems.insertAdjacentHTML("afterbegin",hmtlCarrito)
+  }
+
+  divItems.innerHTML = "";
+  let syntxCarrito = "";
+  let cont = 1;
+ 
+
+  carrito.forEach(car =>{
+   if(mapCarrito.has(car.nombre)){
+      cont = carrito.filter((element) => element["nombre"] === car.nombre).length;
+   }
+   let total = cont*car.precio;
+   let totalProducto = 0;
+
+   syntxCarrito = `<tr>
+                    <td><img src="${car.imagen}"> <label> ${car.nombre}</label></td> 
+                    <td id="cantidad"><i onclick="contador(${cont} +1 , ${car.nombre}, ${car.imagen}, ${car.precio})" class="fa-solid fa-plus bg-success"></i><label>${cont}</label><i onclick="contador(${cont} -1 , ${car.nombre}, ${car.imagen}, ${car.precio})" class="fa-solid fa-minus bg-danger"></i></td>
+                    <td><label><strong>$ ${car.precio}</strong></label></td>
+                    <td><label><strong>$ ${total}</strong></label></td>
+                    </tr>
+                    `
+  mapCarrito.set(car.nombre, syntxCarrito);
+  total = 0;
+  cont = 1;   
+  });
+ 
+ 
+
+  return htmlTable;
+ }
+
+ function contador(contadorActual, nombreMap, imagenMap, precioMap) {
+  let total = contadorActual*precioMap;
+  
+  syntxCarrito = `<tr>
+  <td><img src="${imagenMap}"> <label> ${nombreMap}</label></td> 
+  <td id="cantidad"><i onclick="contador(${contadorActual} +1 , ${nombreMap}, ${imagenMap}, ${precioMap})" class="fa-solid fa-plus bg-success"></i><label>${contadorActual}</label><i onclick="contador(${cont} -1 , ${nombreMap}, ${imagenMap}, ${precioMap})" class="fa-solid fa-minus bg-danger"></i></td>
+  <td><label><strong>$ ${precioMap}</strong></label></td>
+  <td><label><strong>$ ${total}</strong></label></td>
+  </tr>
+  `
+
+ mapCarrito.set(nombreMap, syntxCarrito);
+ total = 0;
+ }
+
+ function almacenamiento(params) {
+  mapCarrito.forEach((map,key) =>{
+    hmtlCarrito += mapCarrito.get(key);
+    console.log(hmtlCarrito);
+  });
+  let htmlTable = `<table id="htmlTable" class="table">
+  <tr>
+    <th scope="col">Producto</th>
+    <th scope="col">Cantidad</th>
+    <th scope="col">Precio</th>
+    <th scope="col">Total</th>
+  </tr>
+  ${hmtlCarrito}
+  </table>`;
+ }
